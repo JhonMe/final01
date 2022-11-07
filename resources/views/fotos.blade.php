cdf3cf7de1062a4c71f815a9592b01216509d85f
JhonMe <1386715@senati.pe>
2022-11-07 10:48:51 -0500

    cuarto

--- resources/views/fotos.blade.php
+++ resources/views/fotos.blade.php
@@ -4,9 +4,78 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Fotos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <style>
+        h5{
+            color:Blue;
+        }
+        img{
+            width: 500px;
+            height: 500px;
+        }
+        h1{
+            color: blue;
+            background: green;
+        }
+        p{
+            color: yellow;
+            font-size:monospace;
+        }
+    </style>
 </head>
 <body>
-    <h1>Aqui las fotos :)</h1>
+    <h1> TE MOSTRANOS ALGUNOS LUGARES</h1>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/07/machu-picchu.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>MACHU PICCHU</h5>
+        <p>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú, sobre el valle del río Urubamba. Se construyó en el siglo XV y luego fue abandonada, y es famosa por sus sofisticadas paredes de piedra seca que combinan enormes bloques sin el uso de un mortero, 
+            los edificios fascinantes que se relacionan con las alineaciones astronómicas y sus vistas panorámicas. El uso exacto que tuvo sigue siendo un misterio..</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.businessempresarial.com.pe/wp-content/uploads/2022/01/version-2018-216.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>HUASCARAN</h5>
+        <p>Es una joya natural que contiene notables ecosistemas y una amplia gama de especies de animales silvestres endémicos propios de la zona. Su protección hace posible la preservación de la gran diversidad biológica, 
+            no sólo contenida dentro del área protegida; sino también la biodiversidad del área circundante..</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://blog.hoteleus.com/wp-content/uploads/2022/01/Vista-de-las-islas-flotantes-de-Uros-con-barcos-tipicos-Puno-Peru.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>LAGO TITI CACA</h5>
+        <p>El Titicaca es un lago de alta montaña que cuenta con una rica biodiversidad, cielos azules y atardeceres de ensueño, y es el hogar de antiguas culturas altoandinas inmersas en las viejas costumbres. 
+            A continuación, te invitamos a conocer más de este lago, también conocido como el Mar de los Andes..</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <div>
-                        <h1>JHON</h1>
+                        <h1>City Tours</h1>
                     </div>
                 </div>
 
