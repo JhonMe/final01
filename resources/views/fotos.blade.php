bb11be9aa6db5acd6129c47e35d0210b3cfa70ca
JhonMe <1386715@senati.pe>
2022-11-08 11:09:31 -0500

    septimo

--- resources/views/fotos.blade.php
+++ resources/views/fotos.blade.php
@@ -23,13 +23,17 @@
         }
         p{
             color: red;
-            line-height: 300%;
+            
         }
     </style>
 </head>
 <body>
     <center>
-       <h1><FONT FACE="small fonts">TE MOSTRAMOS ALGUNO DE LOS LUGARES</FONT></h1>
+       <h1><FONT FACE="small fonts">TE MOSTRAMOS ALGUNO DE LOS LUGARES 
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill" viewBox="0 0 16 16">
+           <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z"/>
+           <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+     </svg></FONT></h1>
     </center>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
@@ -43,7 +47,7 @@
       <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/07/machu-picchu.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
       <FONT FACE="impact" SIZE=6 COLOR="while">MACHU PICCHU</FONT>
-        <p FACE="impact" SIZE=4>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú, sobre el valle del río Urubamba. Se construyó en el siglo XV y luego fue abandonada, y es famosa por sus sofisticadas paredes de piedra seca que combinan enormes bloques sin el uso de un mortero, 
+        <p>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú, sobre el valle del río Urubamba. Se construyó en el siglo XV y luego fue abandonada, y es famosa por sus sofisticadas paredes de piedra seca que combinan enormes bloques sin el uso de un mortero, 
             los edificios fascinantes que se relacionan con las alineaciones astronómicas y sus vistas panorámicas. El uso exacto que tuvo sigue siendo un misterio..</p>
       </div>
     </div>
--- resources/views/ubicaciones.blade.php
+++ resources/views/ubicaciones.blade.php
@@ -5,19 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 <body>
+    <center>
     <div class="card" style="width: 18rem;">
        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <h5 class="card-title">Card title 
+         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+          </svg></h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> 
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="https://www.google.com/maps/place/Machu+Picchu/@-13.1628665,-72.5465275,14.97z/data=!4m5!3m4!1s0x0:0x3a10370ea4a01a27!8m2!3d-13.1631412!4d-72.5449629" class="card-link">Aqui Machu Picchu</a>
      </div>
    </div>
    <div class="card" style="width: 18rem;">
        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Card title 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+            </svg></h5>
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="https://www.google.com/maps/place/Titicaca/@-15.9134984,-69.9612097,9z/data=!3m1!4b1!4m5!3m4!1s0x915d9b22f6af7c75:0xf682724d4df71dc!8m2!3d-15.9254!4d-69.3354" class="card-link">Aqui lago Titicaca</a>
@@ -25,11 +34,18 @@
    </div>
    <div class="card" style="width: 18rem;">
        <div class="card-body">
-        <h5 class="card-title">Card title</h5>
+        <h5 class="card-title">Card title 
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+            </svg></h5>
         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="https://www.google.com/maps/place/Huascar%C3%A1n/@-9.1176707,-77.6058103,12.67z/data=!4m13!1m7!3m6!1s0x91a94787b67d944d:0x92e763d30fd90096!2sHuascar%C3%A1n!3b1!8m2!3d-9.1223815!4d-77.6063478!3m4!1s0x91a94787b67d944d:0x92e763d30fd90096!8m2!3d-9.1223815!4d-77.6063478" class="card-link">Aqui HUASCARAN</a>
      </div>
    </div>
+   </center>
+   <a href="http://127.0.0.1:8000/">
+     <button class="btn btn-primary" type="submit">Volver</button>
+   </a>
 </body>
 </html>
