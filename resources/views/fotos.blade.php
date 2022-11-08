e6d76895bde141a84224108a750b89055ad495c2
JhonMe <1386715@senati.pe>
2022-11-08 10:10:30 -0500

    quinto

--- resources/views/fotos.blade.php
+++ resources/views/fotos.blade.php
@@ -7,45 +7,50 @@
     <title>Fotos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/stiloPrincipal.css">
     <style>
         h5{
             color:Blue;
         }
         img{
-            width: 500px;
-            height: 500px;
+            width: 300px;
+            height: 700px;
         }
         h1{
             color: blue;
-            background: green;
+            background: black;
+            
         }
         p{
-            color: yellow;
-            font-size:monospace;
+            color: red;
+            line-height: 300%;
         }
     </style>
 </head>
 <body>
-    <h1> TE MOSTRANOS ALGUNOS LUGARES</h1>
+    <center>
+       <h1><FONT FACE="small fonts">TE MOSTRAMOS ALGUNO DE LOS LUGARES</FONT></h1>
+    </center>
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+  <center>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/07/machu-picchu.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>MACHU PICCHU</h5>
-        <p>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú, sobre el valle del río Urubamba. Se construyó en el siglo XV y luego fue abandonada, y es famosa por sus sofisticadas paredes de piedra seca que combinan enormes bloques sin el uso de un mortero, 
+      <FONT FACE="impact" SIZE=6 COLOR="while">MACHU PICCHU</FONT>
+        <p FACE="impact" SIZE=4>Machu Picchu es una ciudadela inca ubicada en las alturas de las montañas de los Andes en Perú, sobre el valle del río Urubamba. Se construyó en el siglo XV y luego fue abandonada, y es famosa por sus sofisticadas paredes de piedra seca que combinan enormes bloques sin el uso de un mortero, 
             los edificios fascinantes que se relacionan con las alineaciones astronómicas y sus vistas panorámicas. El uso exacto que tuvo sigue siendo un misterio..</p>
       </div>
     </div>
     <div class="carousel-item">
       <img src="https://www.businessempresarial.com.pe/wp-content/uploads/2022/01/version-2018-216.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>HUASCARAN</h5>
+      <FONT FACE="impact" SIZE=6 COLOR="while">HUASCARAN</FONT>
         <p>Es una joya natural que contiene notables ecosistemas y una amplia gama de especies de animales silvestres endémicos propios de la zona. Su protección hace posible la preservación de la gran diversidad biológica, 
             no sólo contenida dentro del área protegida; sino también la biodiversidad del área circundante..</p>
       </div>
@@ -53,28 +58,19 @@
     <div class="carousel-item">
       <img src="https://blog.hoteleus.com/wp-content/uploads/2022/01/Vista-de-las-islas-flotantes-de-Uros-con-barcos-tipicos-Puno-Peru.jpg" class="d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
-        <h5>LAGO TITI CACA</h5>
+      <FONT FACE="impact" SIZE=6 COLOR="while">LAGO TITI CACA</FONT>
         <p>El Titicaca es un lago de alta montaña que cuenta con una rica biodiversidad, cielos azules y atardeceres de ensueño, y es el hogar de antiguas culturas altoandinas inmersas en las viejas costumbres. 
             A continuación, te invitamos a conocer más de este lago, también conocido como el Mar de los Andes..</p>
       </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  </center>
+  
+  </div>
+  <div>
+     <a href="http://127.0.0.1:8000/">
+     <button type="button" class="btn btn-success">Regresar</button>
+  </a>
   </div>
 </div>
 </body>
--- resources/views/ubicaciones.blade.php
+++ resources/views/ubicaciones.blade.php
@@ -7,6 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>buenas</h1>
+    <div>
+      <a href="https://www.google.com/maps/place/Machu+Picchu/@-13.1628665,-72.5465275,14.97z/data=!4m5!3m4!1s0x0:0x3a10370ea4a01a27!8m2!3d-13.1631412!4d-72.5449629">Puedes ver Aqui Machu Picchu</a>
+    </div>
+    <div>
+       <a href="https://www.google.com/maps/place/Titicaca/@-15.9134984,-69.9612097,9z/data=!3m1!4b1!4m5!3m4!1s0x915d9b22f6af7c75:0xf682724d4df71dc!8m2!3d-15.9254!4d-69.3354">Pudes ver aqui lago Titicaca</a>
+    </div>
+    <div>
+      <a href="https://www.google.com/maps/place/Huascar%C3%A1n/@-9.1176707,-77.6058103,12.67z/data=!4m13!1m7!3m6!1s0x91a94787b67d944d:0x92e763d30fd90096!2sHuascar%C3%A1n!3b1!8m2!3d-9.1223815!4d-77.6063478!3m4!1s0x91a94787b67d944d:0x92e763d30fd90096!8m2!3d-9.1223815!4d-77.6063478">Puedes ver HUASCARAN</a>
+    </div>
 </body>
 </html>
